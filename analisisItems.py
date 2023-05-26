766563e1f419ab5bc4c864317520feb312b958a6
fernandomv3 <fernandomv3@gmail.com>
2023-05-26 17:13:27 -0500

    Usa girth para calcular rasch

--- analisisItems.py
+++ analisisItems.py
@@ -5,13 +5,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import jinja2
-import os
-import subprocess
+#import jinja2
+#import os
 from collections import OrderedDict
-from ctt_and_rasch import score, item_analysis, distractor_analysis, rasch, fit_stats
-
-
+from ctt_and_rasch import score, item_analysis, distractor_analysis, rasch
 
 def leer_estructura(archivo,col_comp='Competencia'):
     comp = {
@@ -79,76 +76,38 @@ def scatter_plot(x,y,labels,colors,cutoff=0.5):
     ax.fill_between(xvals,xvals - cutoff,xvals + cutoff,alpha=0.2)
     return fig
 
-def eRm(X):
-    X.to_csv('matrix.csv')
-    subprocess.run(['Rscript','rasch.R'])
-    difficulty = pd.read_csv('difficulty.csv')['x'].rename('measure')
-    ability = pd.read_csv('ability.csv')['x'].rename('measure')
-    difficulty.index = X.columns
-    ability.index = X.index
-    # Calculate expected values
-    dif,ab = np.meshgrid(difficulty,ability)
-    expected =  np.exp(ab-dif)/(1+np.exp(ab-dif))
-    variances = np.multiply(expected , 1-expected)
-    kurtosis = np.multiply(variances,expected**3 + (1-expected)**3)
-    return difficulty,ability,expected,variances,kurtosis
-
-def raschERm(X):
-    resDif = pd.DataFrame(index=X.columns)
-    resAb = pd.DataFrame(index=X.index)
-
-    resDif = pd.concat([resDif,X.sum().rename('score')],axis=1)
-    resDif["count"] = X.shape[0]
-
-    resAb = pd.concat([resAb,X.sum(axis=1).rename('score')],axis=1)
-    resAb["count"] = X.shape[1]
-
-    difficulty,ability,expected,variances,kurtosis = eRm(X)
-     # Calculate error
-    dif_error = np.sqrt(1/np.sum(variances,axis=0))
-    ab_error = np.sqrt(1/np.sum(variances,axis=1))
-
-    item_fit = fit_stats(X,expected,variances,kurtosis,axis=0)
-    person_fit = fit_stats(X,expected,variances,kurtosis,axis=1)
-
-    dif = pd.DataFrame({'measure':difficulty,'error':dif_error},index=X.columns).join(item_fit)
-    ab = pd.DataFrame({'measure':ability,'error':ab_error},index=X.index).join(person_fit)
-
-    return resDif.join(dif),resAb.join(ab)
-
-def raschWinsteps(X,key,anchors=None):
-    jinja_env = jinja2.Environment(
-        #donde están los templates, por defecto es la carpeta actual
-        loader = jinja2.FileSystemLoader('.'),autoescape= True
-    )
-    tpl = jinja_env.get_template('tpl.con')
-    X.to_csv('data.csv',header=False)
-    confile = tpl.render(
-        path=os.getcwd(),
-        key=''.join(key),
-        anchors = anchors,
-        names=X.columns,
-        nitems=X.shape[1]
-    )
-    with open('test.con','w') as f:
-        f.write(confile)
-    command = f'flatpak run --command=bottles-cli com.usebottles.bottles run -p Winsteps -b Winsteps -- batch=yes "Z:{os.getcwd()}/test.con" "Z:{os.getcwd()}/out.log"'
-    subprocess.run(command,shell=True)
-    dif = pd.read_csv('items.csv',skiprows=1).set_index('NAME')
-    hab = pd.read_csv('person.csv',skiprows=1).set_index('NAME')
-    return dif,hab
+# def raschWinsteps(X,key,anchors=None):
+#     jinja_env = jinja2.Environment(
+#         #donde están los templates, por defecto es la carpeta actual
+#         loader = jinja2.FileSystemLoader('.'),autoescape= True
+#     )
+#     tpl = jinja_env.get_template('tpl.con')
+#     X.to_csv('data.csv',header=False)
+#     confile = tpl.render(
+#         path=os.getcwd(),
+#         key=''.join(key),
+#         anchors = anchors,
+#         names=X.columns,
+#         nitems=X.shape[1]
+#     )
+#     with open('test.con','w') as f:
+#         f.write(confile)
+#     command = f'flatpak run --command=bottles-cli com.usebottles.bottles run -p Winsteps -b Winsteps -- batch=yes "Z:{os.getcwd()}/test.con" "Z:{os.getcwd()}/out.log"'
+#     print(command)
+#     return None,None
 
 @st.cache_data
-def analisisRasch(rsp,key,est,estDTI):
+def analisisRasch(rsp,key,estructura,estructuraDTI):
     scored = score(rsp,key)
     dif = {}
     hab = {}
     graphs = {}
-    estDTI.index = scored.columns
-    for v in est:
-        dif[v],hab[v] = raschERm(scored.filter(regex=f'{v}..'))
-        b_ini = estDTI.loc[estDTI['comp']==v,'Medición']
-        b_calc = dif[v]['measure']
+    estructuraDTI.index = scored.columns
+    for c in estructura:
+        dif[c],hab[c] = rasch(scored.filter(regex=f'{c}..'))
+        
+        b_ini = estructuraDTI.loc[estructuraDTI['comp']==c,'Medición'] #Dificultad de la estructura
+        b_calc = dif[c]['measure'] #Dificultad calculada
 
         # Calcular los items a desanclar
         b_ini0 = b_ini - (b_ini[b_ini.notnull()].mean() - b_calc[b_ini.notnull()].mean())
@@ -156,15 +115,18 @@ def analisisRasch(rsp,key,est,estDTI):
         y = b_calc[b_ini0.notnull()]
         keep = (np.abs(x-y) < 0.5).rename('anchored')
         color = np.where(keep,'blue','red')
-        graphs[v] = scatter_plot(x,y,x.index,color)
+        graphs[c] = scatter_plot(x,y,x.index,color)
 
-        anclas = dif[v].join(keep)['anchored'] == True
+        anclas = dif[c].join(keep)['anchored'] == True
+        #raschWinsteps(rsp.filter(regex=f'{c}..'),key.filter(regex=f'{c}..'),anchors=np.where(anclas,b_ini,np.nan))
 
         diff = b_ini[anclas].mean() - b_calc[anclas].mean()
+        b_calc = np.where(anclas,b_ini,b_calc + diff)
+        #b_calc = b_calc + diff
+        #st.write(b_ini[anclas].mean() - b_calc[anclas].mean())
 
-        b_calc = b_calc + diff
-
-        dif[v] = dif[v].join(anclas).join(b_calc.rename('measureA'))
+        dif[c] = dif[c].join(anclas)
+        dif[c]['measureA'] = b_calc
         
     return dif,hab,graphs
 
@@ -206,6 +168,7 @@ def main():
             if comp:
                 st.pyplot(graphs[comp])
                 st.dataframe(dif[comp],use_container_width=True)
+                #st.dataframe(hab[comp],use_container_width=True)
 
         with tabInsumos:
             st.subheader('Estructura')
